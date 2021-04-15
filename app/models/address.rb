26af9ad0a6c9a4661f379cf78ab026311765ae86
lycho33 <cho.lyds@gmail.com>
2021-04-15 15:41:15 -0400

    commit tree 31f3bfdb74230405247933c82abb53c41f9be3e0

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ActiveRecord::Base
   belongs_to :person
+
+  def address_attributes=(address)
+    self.address = Address.find_or_create_by(
+      street_address_1: address[:street_address_1], 
+      street_address_2: addrress[:street_address_2],
+      city: address[:city],
+      state: address[:state],
+      zipcode: address[:zipcode])
+    self.address.update(address)
+  end
 end
